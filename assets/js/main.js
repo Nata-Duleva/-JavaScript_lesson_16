5cf75380d8e95e803d97cdc3a47f5f0ab5de6e1d
Nata Duleva <nata.duleva@gmail.com>
2020-12-24 23:13:01 +0200

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,8 +210,8 @@ console.log(weekFn('2'));
  * console.log(15, ageClassification(150) === null);
  */
 function ageClassification(n) {
-    return n < 0 ? 'null' : n <= 24 ? 'детский возраст' : n <= 44 ? 'молодой возраст' : n <= 65 ? 'средний возраст'
-        : n <= 75 ? 'пожилой возраст' : n <= 90 ? 'старческий возраст' : n <= 122 ? 'долгожители' : 'null';
+    return n < 0 ? null : n <= 24 ? 'детский возраст' : n <= 44 ? 'молодой возраст' : n <= 65 ? 'средний возраст'
+        : n <= 75 ? 'пожилой возраст' : n <= 90 ? 'старческий возраст' : n <= 122 ? 'долгожители' : null;
 
 }
 
@@ -238,6 +238,25 @@ console.log('90.01:', ageClassification(90.01));
 console.log('122:', ageClassification(122));
 console.log('122.01:', ageClassification(122.01));
 console.log('150:', ageClassification(150));
+
+
+
+console.log('-----------------------------------');
+console.log(1, ageClassification(-1) === null);
+console.log(2, ageClassification(1) === 'детский возраст');
+ console.log(3, ageClassification(24) === 'детский возраст');
+ console.log(4, ageClassification(24.01) === 'молодой возраст');
+ console.log(5, ageClassification(44) === 'молодой возраст');
+ console.log(6, ageClassification(44.01) === 'средний возраст');
+ console.log(7, ageClassification(65) === 'средний возраст');
+ console.log(8, ageClassification(65.01) === 'пожилой возраст');
+ console.log(9, ageClassification(75) === 'пожилой возраст');
+ console.log(10, ageClassification(75.01) === 'старческий возраст');
+ console.log(11, ageClassification(90) === 'старческий возраст');
+ console.log(12, ageClassification(90.01) === 'долгожители');
+ console.log(13, ageClassification(122) === 'долгожители');
+ console.log(14, ageClassification(122.01) === null);
+ console.log(15, ageClassification(150) === null);
 
 /*
  * #7
